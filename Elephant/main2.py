956e6726f029deedce65c68648003a6691d40cb2
Aashutosh-922 <karaleash462@gmail.com>
2023-04-12 04:12:30 +0530

    changesv 12/04

--- Elephant/main2.py
+++ Elephant/main2.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import pygame
-#import time
+import time
 import math
 from mpu6050 import mpu6050
 
@@ -15,6 +15,8 @@ try:
 except:
     print("error")
     pass
+ 
+
 DIR1 = 22
 DIR2 = 31
 DIR3 = 33
@@ -24,14 +26,19 @@ PWM_PIN_2 = 36
 PWM_PIN_3 = 16    #38
 PWM_PIN_4 = 12 #40
 
-DIRB = 7       #claw
-SPDB_PWM_PIN = 13   
 
+URD = 7
+LRD = 13
 
+UR_PWM = 15
+LR_PWM = 29
 
-SHACT = 15
+SHACT = 19 #pneumatics 
 
+CLAW = 21  #claw mechanism
+CLAW_PWM = 23
 
+CSM = 24  #ring pushing
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -47,11 +54,13 @@ GPIO.setup(PWM_PIN_4, GPIO.OUT)
 
 GPIO.setup(SHACT, GPIO.OUT)
 
-GPIO.setup(DIRB, GPIO.OUT)
-GPIO.setup(SPDB_PWM_PIN, GPIO.OUT)
+GPIO.setup(URD, GPIO.OUT)
+GPIO.setup(LRD, GPIO.OUT)
+GPIO.setup(UR_PWM, GPIO.OUT)
+GPIO.setup(LR_PWM, GPIO.OUT)
 
-# GPIO.setup(THRL, GPIO.OUT)
-# GPIO.setup(THRR, GPIO.OUT)
+GPIO.setup(CLAW, GPIO.OUT)
+GPIO.setup(CLAW_PWM, GPIO.OUT)
 
 
 
@@ -61,16 +70,22 @@ SPD2 = GPIO.PWM(PWM_PIN_2, 1000)
 SPD3 = GPIO.PWM(PWM_PIN_3, 1000)
 SPD4 = GPIO.PWM(PWM_PIN_4, 1000)
 
-SPDB = GPIO.PWM(SPDB_PWM_PIN, 1000)
+SPD_UR= GPIO.PWM(UR_PWM, 1000)
+SPD_LR= GPIO.PWM(LR_PWM, 1000)
 
-
+SPD_CLAW = GPIO.PWM(CLAW_PWM, 1000)
+SPD_CSM =  GPIO.PWM(CSM, 1000)
 
 SPD1.start(0)
 SPD2.start(0)
 SPD3.start(0)
 SPD4.start(0)
 
-SPDB.start(0)
+SPD_UR.start(0)
+SPD_LR.start(0)
+
+SPD_CLAW.start(0)
+SPD_CSM.start(0)
 
 # MSPEED = 0
 # PWM = 75
@@ -82,63 +97,63 @@ SPDB.start(0)
 # PWMCHNG = False
 
 
-def set_motor_speed(direction,speed):
+# def set_motor_speed(direction,speed):
 
-    if direction == 'forward':
-        SPD1.ChangeDutyCycle(speed)
-        SPD2.ChangeDutyCycle(speed)
-        SPD3.ChangeDutyCycle(speed)
-        SPD4.ChangeDutyCycle(speed)
-    elif direction == 'backward':
-        SPD1.ChangeDutyCycle(speed)
-        SPD2.ChangeDutyCycle(speed)
-        SPD3.ChangeDutyCycle(speed)
-        SPD4.ChangeDutyCycle(speed)
-    elif direction == 'left':
-        SPD1.ChangeDutyCycle(speed)
-        SPD2.ChangeDutyCycle(0)
-        SPD3.ChangeDutyCycle(speed)
-        SPD4.ChangeDutyCycle(0)
-    elif direction == 'right':
-        SPD1.ChangeDutyCycle(0)
-        SPD2.ChangeDutyCycle(speed)
-        SPD3.ChangeDutyCycle(0)
-        SPD4.ChangeDutyCycle(speed)
-    else:
-        SPD1.ChangeDutyCycle(0)
-        SPD2.ChangeDutyCycle(0)
-        SPD3.ChangeDutyCycle(0)
-        SPD4.ChangeDutyCycle(0)
-# Main loop for motion control
-def mpu():
-    # while True:
-    # Read accelerometer data from MPU6050
-        accelerometer_data = sensor.get_accel_data()
-        x = accelerometer_data['x']
-        y = accelerometer_data['y']
-        z = accelerometer_data['z']
+#     if direction == 'forward':
+#         SPD1.ChangeDutyCycle(speed)
+#         SPD2.ChangeDutyCycle(speed)
+#         SPD3.ChangeDutyCycle(speed)
+#         SPD4.ChangeDutyCycle(speed)
+#     elif direction == 'backward':
+#         SPD1.ChangeDutyCycle(speed)
+#         SPD2.ChangeDutyCycle(speed)
+#         SPD3.ChangeDutyCycle(speed)
+#         SPD4.ChangeDutyCycle(speed)
+#     elif direction == 'left':
+#         SPD1.ChangeDutyCycle(speed)
+#         SPD2.ChangeDutyCycle(0)
+#         SPD3.ChangeDutyCycle(speed)
+#         SPD4.ChangeDutyCycle(0)
+#     elif direction == 'right':
+#         SPD1.ChangeDutyCycle(0)
+#         SPD2.ChangeDutyCycle(speed)
+#         SPD3.ChangeDutyCycle(0)
+#         SPD4.ChangeDutyCycle(speed)
+#     else:
+#         SPD1.ChangeDutyCycle(0)
+#         SPD2.ChangeDutyCycle(0)
+#         SPD3.ChangeDutyCycle(0)
+#         SPD4.ChangeDutyCycle(0)
+# # Main loop for motion control
+# def mpu():
+#     # while True:
+#     # Read accelerometer data from MPU6050
+#         accelerometer_data = sensor.get_accel_data()
+#         x = accelerometer_data['x']
+#         y = accelerometer_data['y']
+#         z = accelerometer_data['z']
 
-        # Calculate pitch and roll angles
-        pitch = 180 * math.atan2(x, math.sqrt(y*y + z*z)) / math.pi
-        roll = 180 * math.atan2(y, math.sqrt(x*x + z*z)) / math.pi
+#         # Calculate pitch and roll angles
+#         pitch = 180 * math.atan2(x, math.sqrt(y*y + z*z)) / math.pi
+#         roll = 180 * math.atan2(y, math.sqrt(x*x + z*z)) / math.pi
 
-        print(pitch)
-        print(roll)
+#         print(pitch)
+#         print(roll)
         
-        # Determine motor direction and speed based on pitch and roll angles
-        if pitch > 7:
-            set_motor_speed('forward', 100)
-        elif pitch < -7:
-            set_motor_speed('backward', 100)
-        elif roll > 14:
-            set_motor_speed('right', 100)
-        elif roll < -14:
-            set_motor_speed('left', 100)
-        else:
-            set_motor_speed('stop', 0)
+#         # Determine motor direction and speed based on pitch and roll angles
+#         if pitch > 7:
+#             set_motor_speed('forward', 100)
+#         elif pitch < -7:
+#             set_motor_speed('backward', 100)
+#         elif roll > 14:
+#             set_motor_speed('right', 100)
+#         elif roll < -14:
+#             set_motor_speed('left', 100)
+#         else:
+#             set_motor_speed('stop', 0)
 
-        # Delay for 0.1 seconds before repeating loop
-        #time.sleep(0.1)
+#         # Delay for 0.1 seconds before repeating loop
+#         #time.sleep(0.1)
 
 
 
@@ -190,11 +205,7 @@ def L(speed): #right
     SPD2.ChangeDutyCycle(speed)
     SPD3.ChangeDutyCycle(speed)
     SPD4.ChangeDutyCycle(speed)
-    
-
-def CLAW():
-	GPIO.output(DIRB, GPIO.HIGH) 
-	
+    	
 
 
 def STOP():
@@ -203,22 +214,29 @@ def STOP():
     GPIO.output(DIR3, GPIO.HIGH)
     GPIO.output(DIR4, GPIO.HIGH)
         
-    GPIO.output(DIRB, GPIO.HIGH)    # AC
+       
     SPD1.ChangeDutyCycle(0)
     SPD2.ChangeDutyCycle(0)
     SPD3.ChangeDutyCycle(0)
     SPD4.ChangeDutyCycle(0)
-    SPDB.ChangeDutyCycle(0)    
+        
 
-# def THROW(speed):
-#     GPIO.output(THRL, GPIO.LOW)    # AC
-#     GPIO.output(THRR, GPIO.LOW)    # AC
-#     THSPDL.ChangeDutyCycle(speed/2)
-#     THSPDR.ChangeDutyCycle(speed)
+def THROW(speed):
+      GPIO.output(URD, GPIO.LOW)    # AC
+      GPIO.output(LRD, GPIO.HIGH)    # C
+      SPD_UR.ChangeDutyCycle(speed)
+      SPD_LR.ChangeDutyCycle(speed)
 
-# MSPEED = 0
-# PWM = 75
-# MAXSPEED = round((PWM/255) * 100)
+MSPEED = 0
+PWM = 75
+MAXSPEED = round((PWM/255) * 100)
+
+PWMPTR = 0
+startThrow = False
+PWMCHNG = False
+btn = None
+
+
 # PWMSPD = [40, 40, 50, 50]
 # # PWMPTRCNT = 3
 # PWMPTR = 0
@@ -285,7 +303,66 @@ while True:
                 STOP()
                 print("Stop 3")
         
+        elif event.type == pygame.JOYBUTTONDOWN:
+            btn = event.button
+        elif event.type == pygame.JOYBUTTONUP:
+            btn = event.button + 20
+
+        if btn != None:
+            if btn == 4: #L1 #throwstart
+                startThrow = True
+                PWMPTR = 0
+        
+            elif btn == 1: #O  #throwstop
+                startThrow = False
+                PWMPTR = 0
+            
+            elif btn == 5:  #pActuator
+                print("SHACT")
+                GPIO.output(SHACT, GPIO.LOW)
+
+                #time.sleep(1)
+                #GPIO.output(SHACT, GPIO.HIGH)
+
+            elif btn == 25:
+                GPIO.output(SHACT, GPIO.HIGH)
+
+            
+            elif btn == 2:#triangle claw down 
+                GPIO.output(CLAW, GPIO.HIGH)
+                SPD_CLAW.ChangeDutyCycle() 
+
+            elif btn == 0: #cross Claw up
+                GPIO.outout(CLAW, GPIO.LOW)
+                SPD_CLAW.ChangeDutyCycle(spd)
+            
+            elif btn == 22 or btn == 20:
+                GPIO.output(CLAW, GPIO.HIGH)
+                #GPIO.output(CLAW, GPIO.LOW)
+                SPD_CLAW.ChangeDutyCycle(0)
+            
+            elif btn == 3:    #servo pushing
+                angle=90
+                duty= (angle/18)+3
+                SPD_CSM.ChangeDutyCycle(duty)
+
+            elif btn == 23:
+                angle=0
+                duty= (angle/18)+3
+                SPD_CSM.ChangeDutyCycle(duty)
+
+             
+
+             
+
+    if(startThrow) :
+        if(MSPEED < MAXSPEED) :
+                #THROW(MAXSPEED)
+            MSPEED += 1
+                  
     
 
+    elif not startThrow and MSPEED > 0 :
+        MSPEED -= 1
     
-
+    THROW(MSPEED)
